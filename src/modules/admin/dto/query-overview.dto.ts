2ca81049a19527ab4b806ad3a7101b231e728d69
JacksonXu <1939377507@qq.com>
2026-07-08 12:26:55 +0800

    修复管理端的admin登录报错问题

---
+++ src/modules/admin/dto/query-overview.dto.ts
@@ -0,0 +1,11 @@
+import { IsOptional, IsDateString } from 'class-validator';
+
+export class QueryOverviewDto {
+  @IsDateString({}, { message: 'startDate 格式无效' })
+  @IsOptional()
+  startDate?: string;
+
+  @IsDateString({}, { message: 'endDate 格式无效' })
+  @IsOptional()
+  endDate?: string;
+}
